661397d3bcb26376287c7ee9b0bb2be80e11dd62
eliasjohansson123 <eliasjohansson9@gmail.com>
2025-10-21 14:37:20 +0200

    Gömde kod i funktioner och gjorde klick-interaktivitet

--- script/script.js
+++ script/script.js
@@ -1,50 +1,92 @@
+// Initierar en lista med klasser som bestämmer rutors egenskaper.
+// Går nog att göras mer läsbart med ett objekt.
 let tileTypes = ["empty", "tableBorder", "bomb", "bombBorder"];
-let tileField = [];
+
+// Initierar ett tomt spelfält
+let tileField;
 const container = document.querySelector("#container");
+
+// Initierar värden som används för att generera bomber
 let bombs = 15;
-let xPos = 0;
-let yPos = 0;
+let bombX;
+let bombY;
 
 // Genererar spelfältet med tommma rutor
-for(let i = 0; i < 12; i++){
-    tileField.push([]);
-    for(let j = 0; j < 12; j++){
-        tileField[i].push(0);
-    }
-}
-// Slumpar fram koordinater för bomber i TileField
-for(let i = 1; i <= bombs; i++){
-    let bombX = Math.floor(Math.random()*10 + 1);
-    let bombY = Math.floor(Math.random()*10 + 1);
-    for(let x = bombX-1; x <= bombX+1; x++){
-        for(let y = bombY - 1; y <= bombY+1; y++){
-            if(tileField[y][x] != 2)
-            tileField[y][x] = 3;
+function generateEmptyField(){
+    tileField = [];
+    for(let i = 0; i < 12; i++){
+        tileField.push([]);
+        for(let j = 0; j < 12; j++){
+            if(i != 0 || i !=12 || j != 0 || j !=12){
+            tileField[i].push(0);
+            }else{
+                tileField[i][j] = 1;
+            }
         }
     }
-    tileField[bombY][bombX] = 2;
 }
-for(i = 0; i < tileField.length; i++){
-    tileField[i][0] = 1;
-    tileField[i][tileField.length - 1] = 1;
-    tileField[tileField.length - 1][i] = 1;
-    tileField[0][i] = 1;
-}
-function revealTiles(y, x){
-    console.log(y, x);
-}
-// Genererar en synlig spelplan utifrån tileField-arrayen mha DOM-manipulation
-function generatePlayArea(){
     
+// Slumpar fram koordinater för bomber i TileField
+let generateBombList = function(){
+    let bombCoordsList = [];
+    for(let i = 1; i <= bombs; i++){
+        bombX = Math.floor(Math.random()*10 + 1);
+        bombY = Math.floor(Math.random()*10 + 1);
+        bombCoordsList.push({x:bombX, y:bombY});
+    }
+    return bombCoordsList;
+}
+function generateBombs(){
+    let bombList = generateBombList();
+    bombList.forEach(el => {
+        for(let i = el.x - 1; i <= el.x + 1; i++){
+            for(let j = el.y - 1; j <= el.y + 1; j++){
+                if(tileField[j][i] != 2){
+                    tileField[j][i] = 3;
+                }
+            }
+        }
+        tileField[el.y][el.x] = 2;
+    });
+}
+
+function floodFill(x,y,twodArr){
+    let thingsToSend = [];
+    let i = x;
+    twodArr[y][x]
+    while(twodArr[y][i] != 2 || twodArr[y][i] != 1){
+        i++;
+        
+    }
+}
+
+// Genererar en synlig spelplan utifrån tileField-arrayen mha DOM-manipulation.
+// Siffrorna i tileField bestämmer vilken klass tile-divvarna kommer att få.
+// Detta bestäms med tileTypes arrayen.
+function generatePlayArea(){
+    generateEmptyField();
+    generateBombs();
+    container.innerHTML = "";
     for(let i = 0; i < 12; i++){
         container.innerHTML += "<div class = 'row'></div>";
         for(let j = 0; j < 12; j++){
-            document.querySelectorAll(".row")[i].innerHTML += `<div class = 'tile ${tileTypes[tileField[i][j]]}'></div>`;
-            document.querySelector(".tile").addEventListener("click", revealTiles(i, j));
+            document.querySelectorAll(".row")[i].innerHTML += `<div data-x=${j} data-y=${i} class = 'tile ${tileTypes[tileField[i][j]]}'></div>`;
         }
     }
+    document.querySelectorAll(".tile").forEach(el => {
+        el.addEventListener("click", revealTiles);
+        el.classList.add("hidden")
+    });
 }
-console.log(tileField);
-console.log(tileField.length);
-console.log(document.querySelectorAll(".tile"))
+function revealTiles (evt){
+    let target = evt.currentTarget;
+    let classArr = target.className.split(" ");
+    let x = target.dataset.x;
+    let y = target.dataset.y;
+    target.classList.remove("hidden");
+    
+
+    console.log(evt.currentTarget);
+    console.log(classArr)
+}
 generatePlayArea();
